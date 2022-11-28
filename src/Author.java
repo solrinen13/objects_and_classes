8bd3eb340b34b38d036c2cfd585edaad68818aa0
Saveliy <saveliy.kan@gmail.com>
2022-11-28 21:22:03 +0300

    2 commit

--- src/Author.java
+++ src/Author.java
@@ -1,27 +1,41 @@
+import java.util.Objects;
 public class Author {
     public String firstNameAuthor;
     public String lastNameAuthor;
 
+
+    Author(String firstNameAuthor, String lastNameAuthor) {
+        this.firstNameAuthor = firstNameAuthor;
+        this.lastNameAuthor = lastNameAuthor;
+    }
     public String getFirstName() {
         return firstNameAuthor;
     }
     public String getLastName() {
         return lastNameAuthor;
     }
-    public String getAuthorFullName() {
+ /*   public String getAuthorFullName() {
         return firstNameAuthor + lastNameAuthor;
-    }
-    String authorFullName = firstNameAuthor + lastNameAuthor;
-    public void setAuthorFullName(String authorFullName) {
-        this.authorFullName = authorFullName;
-    }
+    }*/
+
     public void getInfoAuthor () {
         System.out.print("Фамилия автора " + lastNameAuthor + " имя автора " + firstNameAuthor +" книги в наличие " + 1);
         System.out.println("");
     }
-
-    Author(String firstNameAuthor, String lastNameAuthor) {
-        this.firstNameAuthor = firstNameAuthor;
-        this.lastNameAuthor = lastNameAuthor;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return firstNameAuthor.equals(author.firstNameAuthor) && lastNameAuthor.equals(author.lastNameAuthor);
     }
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstNameAuthor, lastNameAuthor);
+    }
+    @Override
+    public String toString() {
+        return firstNameAuthor + " " + lastNameAuthor;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 public class Main {
+  private static Book[] booksArray = new Book[10];
     public static void main(String[] args) {
-        Book deadSeaScrolls = new Book("Свитки мертвого моря","Человеки", -4000); // Используем созданный конструктор
-        deadSeaScrolls.getInfoBook ();
-        Book steppeWolf = new Book("Степной волк", "Германн Гессе", 1927); // Повторим
+
+        Author author1 = new Author("Герман","Гессе");
+
+
+        Author author2 = new Author("Человеки","");
+
+
+        Book deadSeaScrolls = new Book("Свитки мертвого моря",author2, -4000); // Используем созданный конструктор
+       deadSeaScrolls.getInfoBook ();
+        Book steppeWolf = new Book("Степной волк",author1, 1927); // Повторим
         steppeWolf.getInfoBook ();
-
-        Author Hermann_Hesse = new Author("Герман","Гессе");
-        Hermann_Hesse.getInfoAuthor();
-
-        Author Cheloveki = new Author("Человеки","");
-        Cheloveki.getInfoAuthor();
-
-
+        author1.getInfoAuthor();
 
         deadSeaScrolls.setYearOfPublishing (2021);
         System.out.println("Год издания " +deadSeaScrolls.getYearOfPublishing());
